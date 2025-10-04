a1cb05949cd4eba99e20c41a8838486f82509c7c
swalia <swalia@AD.UCSD.EDU@peregrine.ucsd.edu>
2025-10-04 14:07:44 -0700

    updated script

--- scripts/reproduce_results.sh
+++ scripts/reproduce_results.sh
@@ -6,7 +6,6 @@ tar -xvzf DIPPER_data.tar.gz
 rm DIPPER_data.tar.gz
 
 ############################### AliSim dataset (unaligned sequences) ######################################
-cd DIPPER_data/AliSim
 
 ## Directory structure 
 ## AliSim
@@ -23,31 +22,30 @@ cd DIPPER_data/AliSim
 ##         ├── ..
 ##         └── 10M
 
-dipper -i r -I $PWD/dataset/10K/data.unaligned.fa.gz -O dipper_alisim_10K -m 2
-dipper -i r -I $PWD/dataset/20K/data.unaligned.fa.gz -O dipper_alisim_20K -m 2
-dipper -i r -I $PWD/dataset/50K/data.unaligned.fa.gz -O dipper_alisim_50K -m 1
-dipper -i r -I $PWD/dataset/100K/data.unaligned.fa.gz -O dipper_alisim_100K -m 1
-dipper -i r -I $PWD/dataset/200K/data.unaligned.fa.gz -O dipper_alisim_200K -m 1
-dipper -i r -I $PWD/dataset/500K/data.unaligned.fa.gz -O dipper_alisim_500K -m 1
-dipper -i r -I $PWD/dataset/1M/data.unaligned.fa.gz -O dipper_alisim_1M -m 1
+./dipper -i r -I $PWD/DIPPER_data/AliSim/dataset/10K/data.unaligned.fa.gz -O dipper_alisim_10K -m 2
+./dipper -i r -I $PWD/DIPPER_data/AliSim/dataset/20K/data.unaligned.fa.gz -O dipper_alisim_20K -m 2
+./dipper -i r -I $PWD/DIPPER_data/AliSim/dataset/50K/data.unaligned.fa.gz -O dipper_alisim_50K -m 1
+./dipper -i r -I $PWD/DIPPER_data/AliSim/dataset/100K/data.unaligned.fa.gz -O dipper_alisim_100K -m 1
+./dipper -i r -I $PWD/DIPPER_data/AliSim/dataset/200K/data.unaligned.fa.gz -O dipper_alisim_200K -m 1
+./dipper -i r -I $PWD/DIPPER_data/AliSim/dataset/500K/data.unaligned.fa.gz -O dipper_alisim_500K -m 1
+./dipper -i r -I $PWD/DIPPER_data/AliSim/dataset/1M/data.unaligned.fa.gz -O dipper_alisim_1M -m 1
 
 ### The ultralarge datasets
-#### Placement mode 
-dipper -i r -I $PWD/dataset/ultralarge/200K/data.unaligned.fa.gz -O dipper_alisim_ultralarge_200K -m 1
-dipper -i r -I $PWD/dataset/ultralarge/500K/data.unaligned.fa.gz -O dipper_alisim_ultralarge_500K -m 1
-dipper -i r -I $PWD/dataset/ultralarge/1M/data.unaligned.fa.gz -O dipper_alisim_ultralarge_1M -m 1
-dipper -i r -I $PWD/dataset/ultralarge/2M/data.unaligned.fa.gz -O dipper_alisim_ultralarge_2M -m 1
+#### Placement mode
+./dipper -i r -I $PWD/DIPPER_data/AliSim/dataset/ultralarge/200K/data.unaligned.fa.gz -O dipper_alisim_ultralarge_200K -m 1
+./dipper -i r -I $PWD/DIPPER_data/AliSim/dataset/ultralarge/500K/data.unaligned.fa.gz -O dipper_alisim_ultralarge_500K -m 1
+./dipper -i r -I $PWD/DIPPER_data/AliSim/dataset/ultralarge/1M/data.unaligned.fa.gz -O dipper_alisim_ultralarge_1M -m 1
+./dipper -i r -I $PWD/DIPPER_data/AliSim/dataset/ultralarge/2M/data.unaligned.fa.gz -O dipper_alisim_ultralarge_2M -m 1
 #### Divide-and-conquer mode
-dipper -i r -I $PWD/dataset/ultralarge/200K/data.unaligned.fa.gz -O dipper_alisim_ultralarge_200K_dac -m 3
-dipper -i r -I $PWD/dataset/ultralarge/500K/data.unaligned.fa.gz -O dipper_alisim_ultralarge_500K_dac -m 3
-dipper -i r -I $PWD/dataset/ultralarge/1M/data.unaligned.fa.gz -O dipper_alisim_ultralarge_1M_dac -m 3
-dipper -i r -I $PWD/dataset/ultralarge/2M/data.unaligned.fa.gz -O dipper_alisim_ultralarge_2M_dac -m 3
-dipper -i r -I $PWD/dataset/ultralarge/5M/data.unaligned.fa.gz -O dipper_alisim_ultralarge_5M_dac -m 3
-dipper -i r -I $PWD/dataset/ultralarge/10M/data.unaligned.fa.gz -O dipper_alisim_ultralarge_10M_dac -m 3
+./dipper -i r -I $PWD/DIPPER_data/AliSim/dataset/ultralarge/200K/data.unaligned.fa.gz -O dipper_alisim_ultralarge_200K_dac -m 3
+./dipper -i r -I $PWD/DIPPER_data/AliSim/dataset/ultralarge/500K/data.unaligned.fa.gz -O dipper_alisim_ultralarge_500K_dac -m 3
+./dipper -i r -I $PWD/DIPPER_data/AliSim/dataset/ultralarge/1M/data.unaligned.fa.gz -O dipper_alisim_ultralarge_1M_dac -m 3
+./dipper -i r -I $PWD/DIPPER_data/AliSim/dataset/ultralarge/2M/data.unaligned.fa.gz -O dipper_alisim_ultralarge_2M_dac -m 3
+./dipper -i r -I $PWD/DIPPER_data/AliSim/dataset/ultralarge/5M/data.unaligned.fa.gz -O dipper_alisim_ultralarge_5M_dac -m 3
+./dipper -i r -I $PWD/DIPPER_data/AliSim/dataset/ultralarge/10M/data.unaligned.fa.gz -O dipper_alisim_ultralarge_10M_dac -m 3
 
 
 ############################### RNASim dataset (aligned sequences) ######################################
-cd ../RNASim
     
 ## Directory structure
 ## RNASim
@@ -59,25 +57,23 @@ cd ../RNASim
 ##     ├── ..
 ##     └── 1M
 
-dipper -i m -I $PWD/dataset/10K/data.aligned.fa.gz -O dipper_rnasim_10K -m 2
-dipper -i m -I $PWD/dataset/20K/data.aligned.fa.gz -O dipper_rnasim_20K -m 2
-dipper -i m -I $PWD/dataset/50K/data.aligned.fa.gz -O dipper_rnasim_50K -m 1
-dipper -i m -I $PWD/dataset/100K/data.aligned.fa.gz -O dipper_rnasim_100K -m 1
-dipper -i m -I $PWD/dataset/200K/data.aligned.fa.gz -O dipper_rnasim_200K -m 1
-dipper -i m -I $PWD/dataset/500K/data.aligned.fa.gz -O dipper_rnasim_500K -m 1
-dipper -i m -I $PWD/dataset/1M/data.aligned.fa.gz -O dipper_rnasim_1M -m 1
+./dipper -i m -I $PWD/DIPPER_data/RNASim/dataset/10K/data.aligned.fa.gz -O dipper_rnasim_10K -m 2
+./dipper -i m -I $PWD/DIPPER_data/RNASim/dataset/20K/data.aligned.fa.gz -O dipper_rnasim_20K -m 2
+./dipper -i m -I $PWD/DIPPER_data/RNASim/dataset/50K/data.aligned.fa.gz -O dipper_rnasim_50K -m 1
+./dipper -i m -I $PWD/DIPPER_data/RNASim/dataset/100K/data.aligned.fa.gz -O dipper_rnasim_100K -m 1
+./dipper -i m -I $PWD/DIPPER_data/RNASim/dataset/200K/data.aligned.fa.gz -O dipper_rnasim_200K -m 1
+./dipper -i m -I $PWD/DIPPER_data/RNASim/dataset/500K/data.aligned.fa.gz -O dipper_rnasim_500K -m 1
+./dipper -i m -I $PWD/DIPPER_data/RNASim/dataset/1M/data.aligned.fa.gz -O dipper_rnasim_1M -m 1
 
 
-################################## SILVA datasets ######################################
-cd ../SILVA
-## Directory structure
+################################## SILVA datasets ##################################################
 
 ### lsu subunit
 ### Download sequences
 wget https://www.arb-silva.de/fileadmin/silva_databases/current/Exports/SILVA_138.2_SSURef_tax_silva_full_align_trunc.fasta.gz -O SILVA_138.2_SSURef_tax_silva_full_align_trunc.fasta.gz
-dipper -i m -I $PWD/SILVA_138.2_SSURef_tax_silva_full_align_trunc.fasta.gz -O dipper_silva_lsu -m 1
+./dipper -i m -I $PWD/DIPPER_data/SILVA/SILVA_138.2_SSURef_tax_silva_full_align_trunc.fasta.gz -O dipper_silva_lsu -m 1
 
 ### ssu subunit
 ### Download sequences
 wget https://www.arb-silva.de/fileadmin/silva_databases/current/Exports/SILVA_138.2_LSURef_NR99_tax_silva_full_align_trunc.fasta.gz -O SILVA_138.2_LSURef_NR99_tax_silva_full_align_trunc.fasta.gz
-dipper -i m -I $PWD/SILVA_138.2_LSURef_NR99_tax_silva_full_align_trunc.fasta.gz -O dipper_silva_ssu -m 3
+./dipper -i m -I $PWD/DIPPER_data/SILVA/SILVA_138.2_LSURef_NR99_tax_silva_full_align_trunc.fasta.gz -O dipper_silva_ssu -m 3
